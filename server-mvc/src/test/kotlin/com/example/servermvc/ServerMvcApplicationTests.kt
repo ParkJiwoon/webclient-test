2cea9c38ca304f5271a1d952939e8302b886f7e5
ParkJiwoon <bcp0109@gmail.com>
2022-03-16 21:47:46 +0900

    server-mvc: first commit

---
+++ server-mvc/src/test/kotlin/com/example/servermvc/ServerMvcApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.servermvc
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ServerMvcApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
