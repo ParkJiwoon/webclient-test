9039b8c94f023d0e6806035a87bb1032f34081dc
ParkJiwoon <bcp0109@gmail.com>
2022-03-16 22:18:34 +0900

    server-mvc: Block API 작성
    - 포트 8181 로 띄워서 겹치지 않게 처리

--- server-mvc/src/main/kotlin/com/example/servermvc/ServerMvcApplication.kt
+++ server-mvc/src/main/kotlin/com/example/servermvc/ServerMvcApplication.kt
@@ -1,11 +1,34 @@
 package com.example.servermvc
 
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RestController
 
 @SpringBootApplication
 class ServerMvcApplication
 
+@RestController
+class BlockController {
+
+    companion object {
+        val log: Logger = LoggerFactory.getLogger(BlockController::class.java)
+    }
+
+    @GetMapping("/block/{id}")
+    fun block(@PathVariable id: Long): ResponseEntity<String> {
+        log.info("request $id start")
+        Thread.sleep(5000)
+        log.info("request $id end")
+        return ResponseEntity.ok().body("response $id")
+    }
+}
+
 fun main(args: Array<String>) {
+    System.setProperty("server.port", "8181")
     runApplication<ServerMvcApplication>(*args)
 }
